064211dcdb9c916eab5125d3b85ccf852063a6b2
Bartosz Makoś <loudnownet@gmail.com>
2017-08-23 07:52:50 +0200

    load components per page

--- index.php
+++ index.php
@@ -4,17 +4,24 @@
   <body>
 
     <div class="root">
-      <div class="background -video-8"></div>
+      <div class="background" style="background-image: url(<?php the_field('background', 'option'); ?>)"></div>
       <?php get_template_part('components/header'); ?>
 
       <main class="main">
-        <?php 
+        <?php
+
           if (is_page('home')) {
-            get_template_part('components/quote'); 
-            get_template_part('components/footer');
+            get_template_part('components/quote');
+            get_template_part('components/video-gallery');
           }
-          if (is_page('gallery')) get_template_part('components/gallery-thumbs'); 
-          if (is_page('making-of')) echo 'blog goes here';
+
+          if (is_page('gallery')) get_template_part('components/image-gallery'); 
+
+          // if (is_page('home')) {
+          //    
+          //   get_template_part('components/footer');
+          // }
+          // if (is_page('making-of')) echo 'blog goes here';
         ?>
       </main>
     </div>
